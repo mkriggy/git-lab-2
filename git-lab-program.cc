dbe49717f57774c8c141308b4a9fd5f9540c081e
Michael Krigbaum <mk289117@ohio.edu>
2021-01-26 18:33:07 -0500

    update

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Krigbaum
+ *        Date: January 26, 2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
